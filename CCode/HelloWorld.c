334da3105416944089ff688bea1a1a1003e1a156
Priya <jpriya2903@gmail.com>
2021-06-17 11:37:20 +0530

    Changes to be committed: 	modified:   CCode/HelloWorld.c 	modified:   JavaCode/Hello.java

--- CCode/HelloWorld.c
+++ CCode/HelloWorld.c
@@ -3,11 +3,18 @@
 #include<stdio.h>
 
 void Hello();
+void greeting(char[]);
 
 int main(){
+	char c[] ="Priya";
 	Hello();
 	Hello();
+	greeting(c);
 	return 0;
+}
+
+void greeting(char c[]){
+	printf("Welcome %s\n", c);
 }
 
 void Hello(){	
--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -1,4 +1,7 @@
 public class Hello{
+	public void greetings(String name){
+		System.out.println("Hello" + name);
+	}
 	public static void main(String args[]){
 		System.out.println("Hello World");
 		System.out.println("Hello World");
